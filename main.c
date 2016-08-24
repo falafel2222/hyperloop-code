16f7e77e3fe1b2ac5a732b29c3ea0ec9b2183b45
Alex Goldstein <abg.goldstein@gmail.com>
2016-08-24 13:25:44 -0700

    added mutexes for testing

--- main.c
+++ main.c
@@ -1,42 +1,47 @@
 // Written by OpenLoop Hyperloop Team, August 2016
 
-
 #include "libBBB.h"
 #include <pthread.h>
 #include <semaphore.h>
+
+pthread_mutex_t ledMutex;
 
 
 void *blinky1Func(void *arg) {
 	while(1)
 	{
+		pthread_mutex_lock(&ledMutex);
 		setUsrLedValue(USR1, ON);
-		usleep(2000000);
-		setUsrLedValue(USR1, OFF);
-		usleep(2000000);
+		usleep(1000000);
+		pthread_mutex_unlock();
+		usleep(1000000);
 	}
 }
 
 void *blinky2Func(void *arg) {
 	while(1)
 	{
-		setUsrLedValue(USR3, ON);
+		pthread_mutex_lock(&ledMutex);
+		setUsrLedValue(USR1, OFF);
 		usleep(1000000);
-		setUsrLedValue(USR3, OFF);
+		pthread_mutex_unlock();
 		usleep(1000000);
 	}
 }
 
 int main()
 {
-	
+	pthread_mutex_init(&ledMutex, NULL);
+
 	pthread_t blinky1;
 	pthread_create(&blinky1, NULL, blinky1Func, NULL);
 
 	pthread_t blinky2;
 	pthread_create(&blinky2, NULL, blinky2Func, NULL);
-	
+
 	while(1) {}
+
+	pthread_mutex_destroy(&ledMutex);
 
 	return 0;
 }
-
