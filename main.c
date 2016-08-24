b65dcd939c2475d6ff8446180655951658e723e2
Alex Goldstein <abg.goldstein@gmail.com>
2016-08-24 11:19:06 -0700

    testing out task creation

--- main.c
+++ main.c
@@ -1,43 +1,49 @@
-/*
-//Filename: blinkusrwithlib.c
-//Version : 0.1
-//
-//Project : Argonne National Lab - Forest
-//Author  : Gavin Strunk
-//Contact : gavin.strunk@gmail.com
-//Date    : 14 August 2013
-//
-//Description - This is an example program that demonstrates
-//	how to blink one of the user leds on the beaglebone
-//	black using the libBBB library.
-//
-//Revision History
-//	0.1: Wrote the pauseSec function and the basic program to 
-//		blink the led at 0.5 Hz.  6/28 \GS
-*/
+// Written by OpenLoop Hyperloop Team, August 2016
 
-/*Copyright (C) 2013 Gavin Strunk
 
-Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
-
-//Includes
 #include "libBBB.h"
+#include <pthread.h>
+#include <semaphore.h>
 
-int main()
-{
 
+void *blinky1Func(void *arg) {
 	while(1)
 	{
 		setUsrLedValue(USR3, ON);
-		pauseSec(1);
+		usleep(2);
 		setUsrLedValue(USR3, OFF);
-		pauseSec(1);
+		usleep(2);
 	}
+}
+
+void *blinky2Func(void *arg) {
+	while(1)
+	{
+		setUsrLedValue(USR3, ON);
+		usleep(1);
+		setUsrLedValue(USR3, OFF);
+		usleep(1);
+	}
+}
+
+void createTask(pthread_t handle, pthread_attr_t *att, 
+				void * (*function), void * arg, int priority) {
+	pthread_create(handle, att, function, arg);
+	struct sched_param sp;
+    sp.sched_priority = priority;
+    if(pthread_setschedparam(pthread_self(), SCHED_FIFO, &sp)){
+            fprintf(stderr,"WARNING: Failed to set stepper thread"
+                    "to real-time priority\n");
+    }
+}
+int main()
+{
+	
+	pthread_t blinky1;
+	createTask(&blinky1, NULL, blinky1Func, NULL, 5);
+
+	pthread_t blinky2;
+	createTask(&blinky2, NULL, blinky2Func, NULL, 3);
 
 	return 0;
 }
