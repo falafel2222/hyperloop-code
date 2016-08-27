9c36a88ca951cd8133512b3ed14914f4b7ef2015
Alex Goldstein <abg.goldstein@gmail.com>
2016-08-27 12:35:50 -0700

    Updated timing of tasks

--- main.c
+++ main.c
@@ -2,6 +2,7 @@
 
 #include "libBBB.h"
 #include <pthread.h>
+#include <time.h>
 
 // methods of pod control
 const int TEST_MODE = 1;
@@ -15,6 +16,30 @@ pthread_mutex_t plantCommandMutex;
 pthread_mutex_t podPhaseMutex;
 pthread_mutex_t emergencyFlagMutex;
 
+
+long kalmanStart = 0;
+long photoElectricStart = 0;
+long distanceStart = 0;
+long imuStart = 0;
+long lateralControlStart = 0;
+long brakingStart = 0;
+long dataDisplayStart = 0;
+
+long kalmanPeriod = 1000;
+long photoElectricPeriod = 200;
+long distancePeriod = 5000;
+long imuPeriod = 1000;
+long lateralControlPeriod = 15000;
+long brakingPeriod = 10000;
+long dataDisplayPeriod = 20000;
+
+
+long getTime(){
+	struct timeval currentTime;
+	gettimeofday(&currentTime, NULL);
+	return currentTime.tv_sec * (int)1e6 + currentTime.tv_usec;
+}
+
 double states[10];
 double IMUData[6];
 
@@ -26,50 +51,65 @@ int forcedLateralCorrect;
 
 
 void *kalmanFunction(void *arg) {
+	long timespent = 0; 
 	while(1) {
+		kalmanStart = getTime();
+
 		pthread_mutex_lock(&sensorDataMutex);
 		pthread_mutex_unlock(&sensorDataMutex);
 
 		// Kalman code goes here
 		int i = 0;
-		for (i = 1; i < 200000000; i++) {}
+		for (i = 1; i < 200000; i++) {}
 		
 		pthread_mutex_lock(&statesMutex);
 		pthread_mutex_unlock(&statesMutex);
 
+		timespent = getTime() - kalmanStart;
+		usleep(kalmanPeriod - timespent);
 	}
 }
 
 void *photoElectricFunction(void *arg) {
+	long timespent = 0; 
 	while(1) {
 		// get photoElectric data
 		// calculate stuff
 		pthread_mutex_lock(&sensorDataMutex);
 		pthread_mutex_unlock(&sensorDataMutex);
 
-
+		timespent = getTime() - photoElectricStart;
+		usleep(photoElectricPeriod - timespent);
 	}
 }
 
 void *distanceSensorFunction(void *arg) {
+	long timespent = 0; 
 	while(1) {
 		// get distance sensor data
 		// calculate stuff
 		pthread_mutex_lock(&sensorDataMutex);
 		pthread_mutex_unlock(&sensorDataMutex);
 
+		timespent = getTime() - distanceStart;
+		usleep(distancePeriod - timespent);
 	}
 }
 
 void *imuDataFunction(void *arg) {
+	long timespent = 0; 
 	while(1) {
 		// get IMU data
 		pthread_mutex_lock(&sensorDataMutex);
 		pthread_mutex_unlock(&sensorDataMutex);
+
+		timespent = getTime() - imuStart;
+		usleep(imuPeriod - timespent);
 	}
 }
 
 void *lateralControlFunction(void *arg) {
+	long timespent = 0; 
 	while(1) {
 		pthread_mutex_lock(&emergencyFlagMutex);
 		pthread_mutex_unlock(&emergencyFlagMutex);
@@ -79,10 +119,13 @@ void *lateralControlFunction(void *arg) {
 
 		// determine lateral control plan
 
+		timespent = getTime() - lateralControlStart;
+		usleep(lateralControlPeriod - timespent);
 	}
 }
 
 void *brakingFunction(void *arg) {
+	long timespent = 0; 
 	while(1) {
 		pthread_mutex_lock(&emergencyFlagMutex);
 		pthread_mutex_unlock(&emergencyFlagMutex);
@@ -92,10 +135,13 @@ void *brakingFunction(void *arg) {
 
 		// determine braking
 
+		timespent = getTime() - brakingStart;
+		usleep(brakingPeriod - timespent);
 	}
 }
 
 void *DataDisplayFunction(void *arg) {
+	long timespent = 0; 
 	while(1) {
 
 		pthread_mutex_lock(&sensorDataMutex);
@@ -106,6 +152,8 @@ void *DataDisplayFunction(void *arg) {
 		// display shit
 		pthread_mutex_unlock(&statesMutex);
 
+		timespent = getTime() - dataDisplayStart;
+		usleep(dataDisplayPeriod - timespent);
 	}
 }
 
@@ -144,15 +192,15 @@ int main()
 	pthread_create(&dataDisplay, NULL, DataDisplayFunction, NULL);
 
 
-	setPriority(kalman, 1);
-	setPriority(photoElectric, 1);
-	setPriority(imu, 1);
-	setPriority(distance, 2);
-	setPriority(braking, 2);
-	setPriority(lateralControl, 3);
-	setPriority(dataDisplay, 3);
+	setPriority(kalman, 30);
+	setPriority(photoElectric, 30);
+	setPriority(imu, 30);
+	setPriority(distance, 25);
+	setPriority(braking, 30);
+	setPriority(lateralControl, 25);
+	setPriority(dataDisplay, 15);
 
-	while(1) {
+	while(1){
 
 		usleep(1000);
 	}
